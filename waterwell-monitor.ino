c44a8fb5818e4843c789f9ec9f74307804a4929a
Thomas Theunen <thomas.theunen@forward.eu>
2019-09-08 20:39:27 +0200

    Add task library

--- waterwell-monitor.ino
+++ waterwell-monitor.ino
@@ -1,9 +1,14 @@
 #include <EEPROM.h>
+#include <TaskScheduler.h>
 
 int diameter = 0;
 int depth = 0;
 int distance = 0;
 
+void calculateRemainingLiters();
+
+Task measure(2000, TASK_FOREVER, &calculateRemainingLiters);
+Scheduler runner;
 
 /**
 
@@ -27,6 +32,12 @@ void setup() {
   initDistanceSensor();
   initializeServer();
   
+  runner.init();
+  Serial.println("Initialized scheduler");
+  
+  runner.addTask(measure);
+
+  measure.enable();
 
   Serial.println("Water Well Monitor initialized.");
 }
@@ -36,15 +47,14 @@ void setup() {
 */
 void loop() {
   handleRequests();
-  calculateRemainingLiters();
-  // Serial.print(calculateDistance());
-  // Serial.println("cm ");
+  runner.execute();
 }
 
-int calculateRemainingLiters() {
+void calculateRemainingLiters() {
   int distanceMeasured = calculateDistance();
   int maxLiters = (PI * pow(diameter, 2) * depth)  / 1000;
   int currentLiters = (PI * pow(diameter, 2) * ((depth + distance) - distanceMeasured)) / 1000;
+  
   Serial.println("");
   Serial.print("Measured ");
   Serial.print(distanceMeasured);
